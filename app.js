e97811b4846f9a5b4fbd88f52a8531b7e4de804b
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-07 13:22:41 +0100

    Added the testing controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const middleware = require("./utils/middleware");
 const loginRouter = require("./controllers/login");
 
 const mongoUrl = MONGODB_URI;
+console.log(mongoUrl);
 mongoose
   .connect(mongoUrl)
   .then(() => {
@@ -25,6 +26,12 @@ app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
